f9422c60a5a06a628c346a9f39fbaf0c68f6575e
thoratica <me@tica.fun>
2023-03-14 00:22:32 +0900

    api 수정

--- routes/api/music-via-proxy.ts
+++ routes/api/music-via-proxy.ts
@@ -3,7 +3,9 @@ import { HandlerContext } from "$fresh/server.ts";
 export const handler = async (req: Request, _ctx: HandlerContext): Promise<Response> => {
   const url = new URL(req.url);
 
-  url.hostname = `https://api.allorigins.win/raw?url=${encodeURIComponent(url.hostname)}`;
+  url.hostname = `https://api.allorigins.win/raw?url=${encodeURIComponent(
+    `${url.protocol}//${url.hostname}/api/music${url.search}`
+  )}`;
 
   try {
     const res = await fetch(url);
