b66a395ba0b07ecc864459a1c000482640982bd2
thoratica <me@tica.fun>
2023-03-14 00:46:22 +0900

    api 수정

--- routes/api/music-via-proxy.ts
+++ routes/api/music-via-proxy.ts
@@ -4,33 +4,26 @@ import getAudioUrls from "../../lib/music/getAudioUrls.ts";
 export const handler = async (req: Request, _ctx: HandlerContext): Promise<Response> => {
   try {
     const trackRes = await fetch(
-      `https://api.allorigins.win/raw?url=${encodeURIComponent(req.url.replace("music-via-proxy", "track"))}`
+      `https://api.allorigins.win/raw?url=${encodeURIComponent(
+        req.url.replace("http://localhost:8000", "https://tica.fun").replace("music-via-proxy", "track")
+      )}`
     );
-    if (!trackRes.ok)
-      return Response.json({
-        success: false,
-        message: "error",
-      });
-    const { videoId, name, artists, cover }: { videoId: string; name: string; artists: string; cover: string } =
-      await trackRes.json();
+
+    const {
+      data: { videoId, name, artists, cover },
+    }: { data: { videoId: string; name: string; artists: string; cover: string } } = await trackRes.json();
+
+    console.log(videoId);
 
     const streams = await getAudioUrls(videoId);
     if (!streams) return Response.json({ success: false, message: "stream not found" });
 
     const stream = streams.sort((a, b) => b.bitrate - a.bitrate)[0];
 
-    // return Response.json({ success: true, data: stream.url });
-
     const html = await Deno.readTextFile("music.html");
 
-    // console.log(encodeURIComponent(stream.url));
-
     return new Response(
-      html
-        .replace("$0$", encodeURIComponent(stream.url))
-        .replace("$1$", cover)
-        .replace("$2$", name)
-        .replace("$3$", artists),
+      html.replace("$0$", stream.url).replace("$1$", cover).replace("$2$", name).replace("$3$", artists),
       { headers: { "Content-Type": "text/html" } }
     );
   } catch (err) {
